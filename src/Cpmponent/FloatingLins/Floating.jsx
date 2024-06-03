392cdeb11103de538c592e2f9f67cd532444e59b
ajayrajuput <120632665+ajayrajuput@users.noreply.github.com>
2024-06-03 21:40:03 +0530

    floating links avalible

--- src/Cpmponent/FloatingLins/Floating.jsx
+++ src/Cpmponent/FloatingLins/Floating.jsx
@@ -8,13 +8,13 @@ const Floating = () => {
   };
   return (
     <div className='fixed right-0 top-1/2 transform -translate-y-1/2 flex flex-col space-y-4 px-4 bg-white shadow-xl py-8 rounded-md z-10'>
-      <a href="https://wa.me/1234567890" target="_blank" rel="noopener noreferrer">
+      <a href="https://wa.me/9888885581" target="_blank" rel="noopener noreferrer">
         <img src="./what.svg" alt="What" className="w-7 h-7" />
       </a>
 
       <img src="./mail.svg" alt="Mail" className="w-7 h-7" onClick={contactClick} />
 
-      <a href="tel:91+ 9888885581" target="_blank" rel="noopener noreferrer">
+      <a href="tel:+91 9888885581" target="_blank" rel="noopener noreferrer">
         <img src="./ph.svg" alt="Phone" className="w-7 h-7" />
       </a>
     </div>
